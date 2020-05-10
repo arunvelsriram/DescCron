407565c5b4adf4892f838ccd824cc7553b19088b
Dinesh <dineshudt17@gmail.com>
2020-05-10 14:06:33 +0530

    Remove duplications by passing action,errorMessage as argument to perform function
    Signed-off-by: Dinesh <dineshudt17@gmail.com>

--- src/main/kotlin/dev/arunvelsriram/desccron/MenuItemAction.kt
+++ src/main/kotlin/dev/arunvelsriram/desccron/MenuItemAction.kt
@@ -1,11 +1,15 @@
 package dev.arunvelsriram.desccron
 
+import com.intellij.codeInsight.hint.HintManager
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.PlatformDataKeys
+import com.intellij.openapi.components.service
 import com.intellij.openapi.editor.Editor
 
 abstract class MenuItemAction : AnAction() {
+    private val hintManager = service<HintManager>()
+
     override fun update(e: AnActionEvent) {
         val editor = e.getData(PlatformDataKeys.EDITOR)
         if (getSelectedText(editor).isEmpty()) {
@@ -18,4 +22,19 @@ abstract class MenuItemAction : AnAction() {
     }
 
     fun getSelectedText(editor: Editor?) = editor?.selectionModel?.selectedText?.trim() ?: ""
+
+    fun perform(
+        e: AnActionEvent,
+        action: (String) -> String,
+        errorMessage: String
+    ) {
+        val editor = e.getData(PlatformDataKeys.EDITOR)
+        try {
+            val description = action(getSelectedText(editor))
+            hintManager.showInformationHint(editor!!, description)
+        } catch (e: Exception) {
+            hintManager.showErrorHint(editor!!, e.message ?: errorMessage)
+        }
+    }
+
 }
--- src/main/kotlin/dev/arunvelsriram/desccron/PreviousRunAction.kt
+++ src/main/kotlin/dev/arunvelsriram/desccron/PreviousRunAction.kt
@@ -1,21 +1,13 @@
-package dev.arunvelsriram.desccron;
+package dev.arunvelsriram.desccron
 
-import com.intellij.codeInsight.hint.HintManager
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.PlatformDataKeys
 import com.intellij.openapi.components.service
 
 class PreviousRunAction : MenuItemAction() {
     private val cronDescriptor = service<CronDescriptor>()
-    private val hintManager = service<HintManager>()
 
     override fun actionPerformed(e: AnActionEvent) {
-        val editor = e.getData(PlatformDataKeys.EDITOR)
-        try {
-            val description = cronDescriptor.previousRun(getSelectedText(editor))
-            hintManager.showInformationHint(editor!!, description)
-        } catch (e: Exception) {
-            hintManager.showErrorHint(editor!!, e.message ?: "Failed to get previous run")
-        }
+        val action = { s: String -> cronDescriptor.previousRun(s) }
+        perform(e, action, "Failed to get previous run")
     }
 }
